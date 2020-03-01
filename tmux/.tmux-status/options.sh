76eb966717014c1091632d9dc36b2a4311d75e66
Andrew Thigpen <andyt05@gmail.com>
2020-02-29 19:48:12 -0600

    Upgrade tmux settings to latest.
    Add more status variables as well.

--- tmux/.tmux-status/options.sh
+++ tmux/.tmux-status/options.sh
@@ -4,7 +4,19 @@ flags=""
 
 opt=$(tmux show-window-option synchronize-panes)
 if [[ "$opt" == *"on" ]]; then
-    flags="${flags}s"
+    flags="${flags}sync"
 fi
 
-echo "[$flags]"
+# disk usage
+dfree=$(df --output=pcent / | tail -1)
+dcolor="white,dim"
+[ ${dfree%%%} -gt 85 ] && dcolor="yellow,bold"
+[ ${dfree%%%} -gt 90 ] && dcolor="red,bold"
+
+# mem usage
+mfree=$(free -t | awk 'NR == 2 {printf("%.0f%%", $3/$2*100)}')
+mcolor="white,dim"
+[ ${mfree%%%} -gt 85 ] && mcolor="yellow,bold"
+[ ${mfree%%%} -gt 90 ] && mcolor="red,bold"
+
+echo "  $flags #[default,dim]| #[default] #[fg=$mcolor]$mfree  #[default]#[fg=$dcolor]$dfree #[default]"

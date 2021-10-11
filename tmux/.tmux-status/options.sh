f40ccf46921ff62a5ef930688891808c30d3a98a
Andrew Thigpen <andyt05@gmail.com>
2021-10-11 16:13:26 -0400

    Add toggle-target user systemctl helper tool.

--- tmux/.tmux-status/options.sh
+++ tmux/.tmux-status/options.sh
@@ -9,16 +9,18 @@ fi
 
 # disk usage
 dfree=$(df --output=pcent / | tail -1)
-dcolor="white,dim"
+dcolor="colour245,dim"
 [ ${dfree%%%} -gt 85 ] && dcolor="yellow,bold"
 [ ${dfree%%%} -gt 90 ] && dcolor="red,bold"
 
 # mem usage
 mfree=$(free -t | awk 'NR == 2 {printf("%.0f%%", $3/$2*100)}')
-mcolor="white,dim"
+mcolor="colour245,dim"
 [ ${mfree%%%} -gt 85 ] && mcolor="yellow,bold"
 [ ${mfree%%%} -gt 90 ] && mcolor="red,bold"
 
 targets=$(toggle-target status)
 
-echo "  $flags #[default,dim]|$targets #[default] #[fg=$mcolor]$mfree  #[default]#[fg=$dcolor]$dfree #[default]"
+divider="#[fg=colour237,dim]|#[default]"
+
+echo "  $flags $divider $targets $divider #[fg=$mcolor] $mfree  #[fg=$dcolor]$dfree $divider"

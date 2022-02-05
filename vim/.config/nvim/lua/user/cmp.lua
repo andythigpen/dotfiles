d7e144151a58f1fff7145c2c2c7e5900a372a10e
Andrew Thigpen <andyt05@gmail.com>
2022-02-05 18:18:45 -0500

    LSP signature completion help.

--- vim/.config/nvim/lua/user/cmp.lua
+++ vim/.config/nvim/lua/user/cmp.lua
@@ -53,6 +53,7 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'nvim_lsp_signature_help' },
   }, {
     { name = 'buffer' },
   })

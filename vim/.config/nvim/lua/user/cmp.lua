7a93cbf312fa665b657f4af303b8b8fb2cb66a54
Andrew Thigpen <andyt05@gmail.com>
2022-02-12 15:18:00 -0500

    Fix explicit select of completion items on <CR>.

--- vim/.config/nvim/lua/user/cmp.lua
+++ vim/.config/nvim/lua/user/cmp.lua
@@ -25,7 +25,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ['<C-e>'] = cmp.config.disable,
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
     -- supertab like mappings
     ["<Tab>"] = cmp.mapping(function(fallback)

cdd6e0f3031b895e7a24b8e078ae0d142eebdff8
Andrew Thigpen <andyt05@gmail.com>
2022-02-25 19:39:50 -0500

    Update null-ls formatters.

--- vim/.config/nvim/lua/user/lsp/init.lua
+++ vim/.config/nvim/lua/user/lsp/init.lua
@@ -192,7 +192,8 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.isort.with({ extra_args = { "--profile", "black" } }),
+		null_ls.builtins.formatting.prettier,
 	},
 })
 

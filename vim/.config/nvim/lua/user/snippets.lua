2315a6e88cc41076d55d91acc0017c88af1b9919
Andrew Thigpen <andyt05@gmail.com>
2022-04-16 14:16:48 -0400

    Update snippets configuration to latest.

--- vim/.config/nvim/lua/user/snippets.lua
+++ vim/.config/nvim/lua/user/snippets.lua
@@ -14,44 +14,9 @@ vim.api.nvim_set_keymap("s", "<C-k>", "<Plug>luasnip-jump-prev", {})
 
 -- loads snippets (snipmate format) from directories named "snippets" in the runtimepath
 require("luasnip.loaders.from_snipmate").load()
+require("luasnip.loaders.from_lua").load()
 
--- load snippets defined in lua
--- require('user.snippets.all')
-
-local luasnip = require("luasnip")
-
-function _G.snippets_clear(initial)
-	for m, _ in pairs(luasnip.snippets) do
-		package.loaded["snippets." .. m] = nil
-	end
-	luasnip.snippets = setmetatable({}, {
-		__index = function(t, k)
-			local ok, m = pcall(require, "snippets." .. k)
-			if not ok and not string.match(m, "^module.*not found:") then
-				error(m)
-			end
-			t[k] = ok and m or {}
-
-			-- load snippets from snipmate-library
-			require("luasnip.loaders.from_snipmate").load({ include = { k } })
-			return t[k]
-		end,
-	})
-	if not initial then
-		print("Reloaded snippets")
-	end
-end
-
-_G.snippets_clear(true)
-
-vim.cmd([[
-augroup snippets_clear
-au!
-au BufWritePost */.config/nvim/lua/snippets/*.lua,*/.config/nvim/snippets/*.snippets lua _G.snippets_clear()
-augroup END
-]])
-
-function _G.edit_ft(ext)
+function _G.edit_ft()
 	-- returns table like {"lua", "all"}
 	local fts = require("luasnip.util.util").get_snippet_filetypes()
 	vim.ui.select(fts, {
@@ -59,16 +24,14 @@ function _G.edit_ft(ext)
 	}, function(item, idx)
 		-- selection aborted -> idx == nil
 		if idx then
-			if ext == "lua" then
-				vim.cmd("edit ~/.config/nvim/lua/snippets/" .. item .. ".lua")
-			else
-				vim.cmd("edit ~/.config/nvim/snippets/" .. item .. ".snippets")
-			end
+			vim.cmd("edit ~/.config/nvim/snippets/" .. item .. ".snippets")
 		end
 	end)
 end
 
-vim.cmd([[command! LuaSnipEditLua :lua _G.edit_ft('lua')]])
-vim.cmd([[command! LuaSnipEdit :lua _G.edit_ft('snippets')]])
+vim.cmd([[
+command! LuaSnipEdit :lua _G.edit_ft('snippets')
+command! LuaSnipEditLua :lua require("luasnip.loaders.from_lua").edit_snippet_files()
+]])
 
 -- add custom local snippets below here

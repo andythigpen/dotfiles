f2337c3ae4ea0b50912db0c0af8fef0995c2379d
Andrew Thigpen <andyt05@gmail.com>
2022-04-30 19:31:09 -0400

    Converting vimrc into lua for neovim.

---
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,86 @@
+local keymap = vim.keymap.set
+
+-- toggle spell check (:h spell)
+keymap("n", "<leader>sp", function()
+	vim.wo.spell = not vim.wo.spell
+end)
+
+-- easier navigation between windows
+keymap("n", "<C-J>", "<C-W><C-J>")
+keymap("n", "<C-K>", "<C-W><C-K>")
+keymap("n", "<C-L>", "<C-W><C-L>")
+keymap("n", "<C-H>", "<C-W><C-H>")
+
+-- insert new lines and stay in normal mode
+keymap("n", "zj", "o<Esc>", { silent = true })
+keymap("n", "zk", "O<Esc>", { silent = true })
+
+-- clear the highlighting for the last search
+keymap("n", "<leader>h", ":nohlsearch<CR>", { silent = true })
+-- automatically prepend very-magic search mode to all searches
+keymap("n", "/", "/\\v")
+
+-- switch between last opened buffer
+keymap("n", "<leader><BS>", "<C-^>", { silent = true })
+-- toggle alternate file (ex. switches from .h to .c/cpp/etc)
+keymap("n", "<leader>a", ":A<CR>", { silent = true })
+
+-- quickfix mappings
+
+-- returns true if the quickfix window is open, false otherwise
+local function is_quickfix_open()
+	for i = 1, vim.fn.winnr("$") do
+		if vim.fn.getbufvar(vim.fn.winbufnr(i), "&buftype") == "quickfix" then
+			return true
+		end
+	end
+	return false
+end
+
+-- if the quickfix window is open, navigate in the direction given (one of next or previous).
+-- returns a function intended to be used in a mapping
+local function navigate_quickfix(direction)
+	return function()
+		if not is_quickfix_open() then
+			return
+		end
+		vim.cmd('silent! execute "normal! :c' .. direction .. '<CR>"')
+	end
+end
+
+-- opens the quickfix window or closes it and returns to the previous window if it's currently open
+local function toggle_quickfix()
+	if is_quickfix_open() then
+		vim.cmd("cclose")
+		if vim.g.quickfix_return_to_window then
+			vim.cmd(vim.g.quickfix_return_to_window .. "wincmd w")
+		end
+	else
+		vim.g.quickfix_return_to_window = vim.fn.winnr()
+		vim.cmd([[
+      "Copen! -- requires dispatch
+      copen
+    ]])
+	end
+end
+
+keymap("n", "<leader>q", toggle_quickfix, { silent = true })
+keymap("n", "<leader>[", navigate_quickfix("previous"), { silent = true })
+keymap("n", "<leader>]", navigate_quickfix("next"), { silent = true })
+
+-- toggle line numbers
+local display_columns = true
+local function toggle_columns()
+	if display_columns then
+		vim.opt.signcolumn = "no"
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+		display_columns = false
+	else
+		vim.opt.signcolumn = "yes"
+		vim.opt.number = true
+		vim.opt.relativenumber = true
+		display_columns = true
+	end
+end
+keymap("n", "<leader>n", toggle_columns, { silent = true })
---
+++ neovim/.config/nvim/lua/user/options.lua
@@ -0,0 +1,72 @@
+--
+-- init.lua
+-- Settings are divided into groups according to the output of :options
+-- The number for each section corresponds to the section number of :options
+-- Most of the help comments beside each option come from :options as well
+--
+
+local opt = vim.opt
+local g = vim.g
+
+opt.pastetoggle = "<leader>p"
+
+-- 2 Moving around, searching and patterns
+opt.ignorecase = true -- ignore case when using a search pattern
+opt.smartcase = true -- override 'ignorecase' when pattern has upper case characters
+
+-- 4 Displaying text
+opt.scrolloff = 3 -- number of screen lines to show around the cursor
+opt.linebreak = true -- wrap long lines at a character in 'breakat'
+opt.lazyredraw = true -- don't redraw while executing macros
+opt.number = true -- print the line number for each line
+opt.relativenumber = true -- show line numbers
+opt.numberwidth = 5 -- line number gutter width
+opt.cmdheight = 2 -- height of the bottom cmd bar in lines
+opt.fillchars = "fold:─" -- characters to use for statusline, folds, filler lines
+
+-- 5 Syntax, highlighting, and spelling
+opt.colorcolumn = "101" -- highlight the 101 column boundary
+opt.termguicolors = true
+
+-- 6 Multiple windows
+opt.laststatus = 3 -- enable global statusline
+
+-- 11 Messages and info
+opt.shortmess:append({ c = true }) -- don't give |ins-completion-menu| messages
+
+-- 12 Selecting text
+-- requires tmux 3.2+, also only want to set when using tmux
+local tmux_version = vim.split(vim.env.TMUX_VERSION or "", ".", { plain = true })
+if #tmux_version >= 2 and tonumber(tmux_version[1]) >= 3 and tonumber(tmux_version[2]) >= 2 then
+	opt.clipboard = "unnamedplus"
+	g.clipboard = {
+		name = "tmux-osc52",
+		copy = {
+			["+"] = { "tmux", "load-buffer", "-w", "-" },
+			["*"] = { "tmux", "load-buffer", "-w", "-" },
+		},
+		paste = {
+			["+"] = { "tmux", "save-buffer", "-" },
+			["*"] = { "tmux", "save-buffer", "-" },
+		},
+		["cache-enabled"] = 1,
+	}
+end
+
+-- 13 Editing text
+opt.undofile = true -- save undo history to a file
+opt.completeopt:remove({ "preview" }) -- do not display a preview window on autocomplete
+opt.completeopt:append({ "noselect", "menuone" }) -- do not select the first item (noselect), display the menu even if only one match (menuone)
+
+-- 14 Tabs and indenting
+opt.tabstop = 4 -- number of spaces a <Tab> in the text stands for
+opt.shiftwidth = 4 -- number of spaces used for each step of (auto)indent
+opt.softtabstop = 4 -- if non-zero, number of spaces to insert for a <Tab>
+opt.shiftround = true -- round to 'shiftwidth' for "<<" and ">>"
+opt.expandtab = true -- expand <Tab> to spaces in Insert mode
+
+-- 20 Command line editing
+opt.wildignore:append({ "*.pyc", "*.o", "*.obj", "*.swp" })
+
+-- 25 Various
+opt.signcolumn = "yes"

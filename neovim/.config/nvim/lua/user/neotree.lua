50a7b971ea1bf524ba97527b19c5eb4dd0313b29
Andrew Thigpen <andyt05@gmail.com>
2022-07-03 12:05:20 -0400

    Change neo-tree popup border style.

--- neovim/.config/nvim/lua/user/neotree.lua
+++ neovim/.config/nvim/lua/user/neotree.lua
@@ -4,6 +4,7 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 require("neo-tree").setup({
 	enable_diagnostics = false,
+	popup_border_style = "single",
 	window = {
 		mappings = {
 			["x"] = "close_node",

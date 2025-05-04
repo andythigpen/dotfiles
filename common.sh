c86f8f6d9b863088dade9b671bb74e43ba149273
Andrew Thigpen <andyt05@gmail.com>
2025-05-04 10:21:33 -0400

    Fix shellcheck issues.

--- common.sh
+++ common.sh
@@ -4,26 +4,28 @@
 #
 
 info() {
-    printf "\r  [\033[00;34mINFO\033[0m] $1\n"
+    printf "\r  [\033[00;34mINFO\033[0m] %s\n" "$1"
 }
 
 ask() {
-    printf "\r  [ \033[0;33m??\033[0m ] $1 "
-    read $2
+    printf "\r  [ \033[0;33m??\033[0m ] %s " "$1"
+    read -r "$2"
 }
 
 # prompts the user to make a y/n choice
 # 1: prompt text
 # 2: default answer (one of 'y' or 'n')
 confirm() {
-    local default=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+    local default
+    default=$(echo "$2" | tr '[:upper:]' '[:lower:]')
     [ -z "$default" ] && default="y"
 
-    local choices=$(echo '[y/n]' | tr $default $(echo $default | tr '[:lower:]' '[:upper:]'))
+    local choices
+    choices=$(echo '[y/n]' | tr "$default" "$(echo "$default" | tr '[:lower:]' '[:upper:]')")
 
     while true; do
-        printf "\r  [ \033[0;33m??\033[0m ] $1 $choices "
-        read choice
+        printf "\r  [ \033[0;33m??\033[0m ] %s %s " "$1" "$choices"
+        read -r choice
         choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
         [ -z "$choice" ] && choice=$default
         [ "$choice" = "y" ] && return 0
@@ -33,16 +35,15 @@ confirm() {
 }
 
 success() {
-    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
 }
 
 warn() {
-    printf "\r\033[2K  [\033[0;31mWARN\033[0m] $1\n"
+    printf "\r\033[2K  [\033[0;31mWARN\033[0m] %s\n" "$1"
 }
 
 fail() {
-    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
     echo ''
     exit
 }
-
--- install.sh
+++ install.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OS=$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"')
 
-source $DIR/common.sh
+source "$DIR"/common.sh
 
 DEPS=(stow silversearcher-ag build-essential cmake python3-dev ripgrep fzf)
 
 if [ "$OS" = "Ubuntu" ]; then
-    info "Installing package dependencies"
-    sudo apt install ${DEPS[*]}
+    if confirm "Install package dependencies?"; then
+        info "Installing package dependencies"
+        sudo apt install "${DEPS[*]}"
+    fi
 else
     warn "Dependency installation not supported for $OS."
     warn "The following requirements should be installed manually:"
     warn ""
     warn "${DEPS[*]}"
-    read -n1 -r -p 'Press any key to continue...' key
+    read -n1 -r -p 'Press any key to continue...'
 fi
 
 if [ "$#" -eq 0 ]; then
     info "Installing all packages"
-    PKGS=$(ls -d */)
+    PKGS=$(ls -d ./*/)
     SHOULD_CONFIRM=1
 else
-    PKGS="${@}"
+    PKGS="${*}"
     SHOULD_CONFIRM=0
 fi
 
 git submodule update --init
 
-pushd $DIR &>/dev/null
+pushd "$DIR" &>/dev/null || exit
 for pkg in $PKGS; do
     pkg=${pkg%%/}
     if [ $SHOULD_CONFIRM -eq 1 ] && ! confirm "Install $pkg package?"; then
@@ -40,8 +42,7 @@ for pkg in $PKGS; do
         info "Running pre install for $pkg"
         bash "$pkg/pre-install.sh" || fail "Pre-install failed for $pkg"
     fi
-    stow --ignore=pre-install.sh --ignore=post-install.sh -v $pkg
-    if [ $? -eq 0 ]; then
+    if stow --ignore=pre-install.sh --ignore=post-install.sh -v "$pkg"; then
         if [ -f "$pkg/post-install.sh" ]; then
             info "Running post install for $pkg"
             bash "$pkg/post-install.sh" || fail "Post-install failed for $pkg"
@@ -51,4 +52,4 @@ for pkg in $PKGS; do
         fail "Failed to install $pkg"
     fi
 done
-popd &>/dev/null
+popd &>/dev/null || exit
